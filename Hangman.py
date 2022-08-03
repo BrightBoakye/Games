ac953558b9a31229b3697521621704e3878a25bb
TheBase99 <87952798+TheBase99@users.noreply.github.com>
2022-08-03 02:08:16 +0000

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -51,11 +51,7 @@ words = {'Colors':'red orange yellow green blue indigo violet white black brown'
 'Animals':'bat bear beaver cat cougar crab deer dog donkey duck eagle fish frog goat leech lion lizard monkey moose mouse otter owl panda python rabbit rat shark sheep skunk squid tiger turkey turtle weasel whale wolf wombat zebra'.split()}
 
 def getRandomWord(wordDict):
-    # This function returns a random string from the passed dictionary of lists of strings, and the key also.
-    # First, randomly select a key from the dictionary:
     wordKey = random.choice(list(wordDict.keys()))
-
-    # Second, randomly select a word from the key's list in the dictionary:
     wordIndex = random.randint(0, len(wordDict[wordKey]) - 1)
 
     return [wordDict[wordKey][wordIndex], wordKey]
@@ -71,16 +67,15 @@ def displayBoard(missedLetters, correctLetters, secretWord):
 
     blanks = '_' * len(secretWord)
 
-    for i in range(len(secretWord)): # replace blanks with correctly guessed letters
+    for i in range(len(secretWord)): 
         if secretWord[i] in correctLetters:
             blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
 
-    for letter in blanks: # show the secret word with spaces in between each letter
+    for letter in blanks: 
         print(letter, end=' ')
     print()
 
 def getGuess(alreadyGuessed):
-    # Returns the letter the player entered. This function makes sure the player entered a single letter, and not something else.
     while True:
         print('Guess a letter.')
         guess = input()
@@ -95,7 +90,6 @@ def getGuess(alreadyGuessed):
             return guess
 
 def playAgain():
-    # This function returns True if the player wants to play again, otherwise it returns False.
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
@@ -124,13 +118,11 @@ while True:
     print('The secret word is in the set: ' + secretSet)
     displayBoard(missedLetters, correctLetters, secretWord)
 
-    # Let the player type in a letter.
+    # type in a letter.
     guess = getGuess(missedLetters + correctLetters)
 
     if guess in secretWord:
         correctLetters = correctLetters + guess
-
-        # Check if the player has won
         foundAllLetters = True
         for i in range(len(secretWord)):
             if secretWord[i] not in correctLetters:
